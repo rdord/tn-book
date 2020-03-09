8922dc5949668894ce8aac75528aa04e893c2389
Roland Đorđević <roland.djordjevic@ineor.si>
2020-03-09 19:16:28 +0100

    add one unavailable time as an object

--- src/stores/AppointmentStore.js
+++ src/stores/AppointmentStore.js
@@ -1,12 +1,22 @@
 import { createContext } from 'react';
-import { decorate, observable } from 'mobx';
-import { addMonths, subMonths, set, format, parse } from 'date-fns';
+import { decorate, observable, computed, toJS } from 'mobx';
+import { addMonths, subMonths, set, format, parse, startOfMonth, startOfWeek, endOfMonth, endOfWeek } from 'date-fns';
 
 export class AppointmentStore {
   selectedTime = set(new Date(), { hours: 0 });
   unavailableTimes = [];
   isAdmin = true;
   appointments = [];
+
+  get startDate() {
+    const monthStart = startOfMonth(this.selectedTime);
+    return startOfWeek(monthStart);
+  }
+
+  get endDate() {
+    const monthEnd = endOfMonth(this.selectedTime);
+    return endOfWeek(monthEnd);
+  }
 
   // UI
   toggleAdmin = () => (this.isAdmin = !this.isAdmin);
@@ -21,7 +31,26 @@ export class AppointmentStore {
   removeAppointment = appt => (this.appointments = this.appointments.filter(a => a !== appt));
 
   // TODO: refactor to an {day: format(selectedDay, 'D'), times: []}
-  addUnavailableTime = time => this.unavailableTimes.push(time);
+  addUnavailableTime = time => {
+    const day = format(time, 'DDD');
+    let dayExists = false;
+
+    const updatedDay = this.unavailableTimes.map(obj => {
+      if (obj.day === day) {
+        dayExists = true;
+        obj.times.push(time);
+      }
+      return obj;
+    });
+
+    if (dayExists) {
+      this.unavailableTimes = updatedDay;
+    } else {
+      this.unavailableTimes.push({ day, times: [time] });
+    }
+
+    console.log('addUnavailableTime', toJS(this.unavailableTimes));
+  };
   removeUnavailableTime = time => (this.unavailableTimes = this.unavailableTimes.filter(t => t !== time));
 
   addUnavailableTimes = times => {
@@ -31,6 +60,8 @@ export class AppointmentStore {
     const uniqueDates = uniqueStrings.map(t => parse(t, 'd. MMM yyyy - HH:mm', new Date()));
 
     this.unavailableTimes = uniqueDates;
+
+    console.log(toJS(this.unavailableTimes));
   };
 }
 
@@ -38,7 +69,9 @@ decorate(AppointmentStore, {
   selectedTime: observable,
   unavailableTimes: observable,
   isAdmin: observable,
-  appointments: observable
+  appointments: observable,
+  startDate: computed,
+  endDate: computed
 });
 
 export default createContext(new AppointmentStore());
